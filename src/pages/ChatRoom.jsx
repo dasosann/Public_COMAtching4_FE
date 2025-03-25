2c490bb77d4d01d74c97ee55231b895906ee6ceb
ojspp41 <ojspp000@naver.com>
2025-03-25 19:26:57 +0900

    아래 마진 추가가

--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -10,7 +10,25 @@ const chatMessages = [
     { id: 2, sender: "other", message: "반갑습니다 :)", time: "10:01" },
     { id: 3, sender: "me", message: "오늘 날씨 좋네요", time: "10:02" },
     { id: 4, sender: "other", message: "네! 산책 가고 싶어요", time: "10:03" },
+    { id: 5, sender: "me", message: "보통 주말에 뭐 하세요?", time: "10:04" },
+    { id: 6, sender: "other", message: "카페 가거나 영화 봐요!", time: "10:05" },
+    { id: 7, sender: "me", message: "오 저도 영화 좋아해요. 최근에 뭐 보셨어요?", time: "10:06" },
+    { id: 8, sender: "other", message: "‘듄 2’ 봤어요! 너무 재밌었어요", time: "10:07" },
+    { id: 9, sender: "me", message: "오 저도 그거 궁금했는데 평이 좋더라고요", time: "10:08" },
+    { id: 10, sender: "other", message: "액션이랑 음악이 진짜 대박이에요", time: "10:09" },
+    { id: 11, sender: "me", message: "좋아요! 이번 주말에 꼭 볼게요", time: "10:10" },
+    { id: 12, sender: "me", message: "혹시 좋아하는 음식 있으세요?", time: "10:11" },
+    { id: 13, sender: "other", message: "요즘 초밥에 꽂혔어요 🍣", time: "10:12" },
+    { id: 14, sender: "me", message: "헉 저도! 연어초밥 최고죠", time: "10:13" },
+    { id: 15, sender: "other", message: "ㅋㅋㅋ 완전 동감입니다!", time: "10:14" },
+    { id: 16, sender: "me", message: "혹시 취미는 뭐예요?", time: "10:15" },
+    { id: 17, sender: "other", message: "그림 그리는 거요. 힐링돼요", time: "10:16" },
+    { id: 18, sender: "me", message: "멋지다! 언젠가 작품도 보여주세요", time: "10:17" },
+    { id: 19, sender: "other", message: "기회 되면 꼭 보여드릴게요 😊", time: "10:18" },
+    { id: 20, sender: "me", message: "좋은 하루 보내세요~", time: "10:19" },
+    { id: 21, sender: "other", message: "네! 오늘도 파이팅이에요 💪", time: "10:20" }
 ];
+  
 
 function ChatRoom() {
     const [focused, setFocused] = useState(false);
@@ -30,6 +48,7 @@ function ChatRoom() {
                         time={chat.time} />
                 ))}
             </div>
+            <div  style={{ height: '100px' }}></div>
 
             <div className="chat-input-container">
                 <div
