53532f5e66b359f1a2ec2ae92279a6505736a779
dasosann <goodtony0403@gmail.com>
2025-04-16 15:22:07 +0900

    add 매칭 상대 조회하기 ( 상대가 없을때 )

---
+++ src/css/pages/MyPageSearch/SearchMyPageStyle.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+const E = {};
+E.MainContainer = styled.div`
+    padding: 0 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+E.BackArrowDiv = styled.div`
+    width: 100%;
+    text-align: start;
+    margin-top: 1.72vh;
+`
+E.HeaderContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.92vh;
+`
+E.MainSpan = styled.span`
+    font-size: 20px;
+    font-weight: 700;
+    color: #373737;
+`
+E.Subspan = styled.div`
+    font-size: 14px;
+    color: #858585;
+    font-weight: 500;
+`
+E.MainWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 22.20vh;
+    width: 100%;
+    height: 21.05vh;
+    display: flex;
+    flex-direction: column;
+    gap: 1.83vh;
+`
+E.MatchingDiv = styled.div`
+    line-height: 160%;
+    width: 100%;
+    text-align: center;
+    font-size: 14px;
+    font-weight: 500;
+    word-break: keep-all;
+`
+E.HightlightSpan = styled.span`
+    font-size: #ff4d61;
+`
+E.MatchingButtonDiv = styled.div`
+    width: 136px;
+    height: 38px;
+    border-radius: 99px;
+    padding: 16px 8px;
+    background: conic-gradient(from -36.07deg at 64.06% -102.34%, #E83ABC 0deg, rgba(255, 119, 94, 0.1) 0.04deg, rgba(255, 77, 97, 0.6) 169.2deg, #E83ABC 360deg);
+    color: #fff;
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    
+`
+export default E;
+
---
+++ src/pages/SearchMyPage/SearchMyPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Background from '../../components/Background';
+import E from '../../css/pages/MyPageSearch/SearchMyPageStyle'
+const SearchMyPage = () => {
+    return (
+        <E.MainContainer>
+            <Background/>
+            <E.BackArrowDiv>
+                <img src="/assets/MainPayment/arrow-left.svg" alt="화살표" />
+            </E.BackArrowDiv>
+            <E.HeaderContainer>
+                <E.MainSpan>조회하기</E.MainSpan>
+                <div>
+                    <E.Subspan>내가 뽑은 상대들을 여기서 확인할 수 있어요.</E.Subspan>
+                    <E.Subspan>용기내서 연락해보세요!</E.Subspan>
+                </div>
+            </E.HeaderContainer>
+            <E.MainWrapper>
+                <img src="/assets/character-logo.svg" alt="캐릭터" />
+                <E.MatchingDiv>
+                    아직 매칭된 상대가 없어요. <br/> 아직 <E.HightlightSpan>732</E.HightlightSpan>명이 겨울이오길님을 기다리고 있어요.<br/>나와 딱 맞는 이성친구를 만들어봐요!                
+                </E.MatchingDiv>
+                <E.MatchingButtonDiv>
+                    <span>매칭하러 가기</span>
+                    <img src="/assets/white-arrow.svg" alt="화살표" />
+                </E.MatchingButtonDiv>
+            </E.MainWrapper>
+
+        </E.MainContainer>
+    );
+};
+
+export default SearchMyPage;
