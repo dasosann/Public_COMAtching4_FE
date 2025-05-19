b5e18d9fcf824f9bd3708b20f343766fb752a004
ojspp41 <ojspp000@naver.com>
2025-05-19 21:49:20 +0900

    websocket false complete

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.ts 또는 vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
@@ -5,12 +6,19 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 export default defineConfig({
   server: {
     historyApiFallback: true,
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost', // 또는 실제 개발서버의 IP
+      port: 5173,
+    },
   },
   plugins: [
     react(),
     vanillaExtractPlugin(),
   ],
   define: {
-    global: "window", // global을 window 객체로 매핑
+    global: "window",
   },
 });
