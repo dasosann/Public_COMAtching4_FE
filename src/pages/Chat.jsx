0c89de6c9227832923dff2b06964d46252d9f235
ojspp41 <ojspp000@naver.com>
2025-05-19 20:27:48 +0900

    use

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/pages/ChatList.css';
 import Background from '../components/Background';
