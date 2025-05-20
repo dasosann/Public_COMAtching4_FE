5a4121efd0ea80f9c131b43f0e651d9df65842b7
ojspp41 <ojspp000@naver.com>
2025-05-20 13:49:08 +0900

    css

--- src/components/Chat/ChatItem.jsx
+++ src/components/Chat/ChatItem.jsx
@@ -6,10 +6,12 @@ function ChatItem({ name, age, major }) {
       <div className="chat-info">
         <div className="chat-top">
           <div className="chat-name">{name}</div>
-          <div className="chat-age">{age}살</div>
+          <div className="chat-age">{age}</div>
           <div className="chat-major">{major}</div>
         </div>
-        
+        <div className="chat-bottom">
+            채팅을 눌러서 시작하세요!
+        </div>
       </div>
     </div>
   );
