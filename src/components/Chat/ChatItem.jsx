4690cb245978415a6392b1a9df979c76add21b8d
ojspp41 <ojspp000@naver.com>
2025-05-20 13:44:47 +0900

    실패패

--- src/components/Chat/ChatItem.jsx
+++ src/components/Chat/ChatItem.jsx
@@ -6,7 +6,7 @@ function ChatItem({ name, age, major }) {
       <div className="chat-info">
         <div className="chat-top">
           <div className="chat-name">{name}</div>
-          <div className="chat-age">{age}</div>
+          <div className="chat-age">{age}살</div>
           <div className="chat-major">{major}</div>
         </div>
         
