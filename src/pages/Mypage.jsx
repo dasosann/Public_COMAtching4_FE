1a168dd627a698201159642a61c388582e308a17
dasosann <goodtony0403@gmail.com>
2025-05-21 17:47:19 +0900

    충전막기

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -19,6 +19,9 @@ const Mypage = () => {
   const openModal = () => {
     setIsModalOpen(true);
   };
+  const handleNotService = () => {
+        alert('해당 서비스는 5/22일 10:00에 오픈됩니다 축제까지 기다려주세요!');
+    };
   useEffect(() => {
     // ✅ 사용자 프로필 정보 요청
     const fetchUserProfile = async () => {
@@ -60,7 +63,7 @@ const Mypage = () => {
                 <p className='mypage-point-label'>보유 포인트</p>
                 <p className='mypage-point-value'>{userData.point.toLocaleString()} P</p>
             </div>
-            <button className='mypage-recharge-btn' onClick={openModal}>충전하기</button>
+            <button className='mypage-recharge-btn' onClick={handleNotService}>충전하기</button>
             
         </div>
         {/* <div className='mypage-history-btns'>
